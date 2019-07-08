cf539862fe70168e1d0d598dcbf92aaa99901ae0
lucas von lachmann <lucasvonlachmann@hotmail.com>
2019-07-07 21:22:29 -0300

    scanning qrcode

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :vouchers
   devise_for :users
   root to: 'pages#home'
+
+  get '/scanqr', to: 'vouchers#scanqr'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
